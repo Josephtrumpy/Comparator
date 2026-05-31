51615de98ca7791ed5f9568d7385c8a40f607a07
Joseph Trumpy <148014641+Josephtrumpy@users.noreply.github.com>
2026-05-31 13:11:28 -0500

    Delete IncomingStudent.java

--- IncomingStudent.java
+++
@@ -1,7 +0,0 @@
-public class IncomingStudent implements Comparable<IncomingStudent> {
-
-    @Override
-    public int compareTo(IncomingStudent other) {
-        return this.name.compareTo(other.name);
-    }
-}
